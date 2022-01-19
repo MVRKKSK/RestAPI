473a701e02eb69e86d5d432a0aaabe3a02c20e55
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-01-19 22:25:34 +0530

    created post request from postman

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,19 @@
 const express = require("express");
+const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 
 
-const users = [
-    {
-        firstname: "uzumaki",
-        lastname: "naruto",
-        age: 30
-    }
-]
+const users = [];
 
-router.get("/" , (req,res) => {
+router.get("/", (req, res) => {
     res.send(users);
 
 });
 
-router.post("/" , (req,res)=>{
+router.post("/", (req, res) => {
     const user = req.body;
-    users.push(user);
+
+    users.push({ ...user, id: uuidv4() });
     res.send(`User with the name ${user.firstname} was added to the database`)
 
 });
