b0ed9419934959aa9c86a9508ec2dd31807724d6
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-01-20 12:03:06 +0530

    completed all the crud operations in postman

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 
 
-const users = [];
+let users = [];
 
 router.get("/", (req, res) => {
     res.send(users);
@@ -18,16 +18,40 @@ router.post("/", (req, res) => {
 
 });
 
-router.get("/:id" , (req,res) => {
+router.get("/:id", (req, res) => {
 
     const { id } = req.params;
     const FindByid = users.find((user) => {
-        if(user.id === id){
-            return(user)
+        if (user.id === id) {
+            return (user)
         }
     })
-
     res.send(FindByid)
+})
+
+router.delete("/:id", (req, res) => {
+
+    const { id } = req.params;
+    users = users.filter((user) =>
+        user.id !== id
+    )
+
+    res.send(`the user ${id} deleted from the database`)
+})
+
+router.patch("/:id", (req, res) => {
+    const { id } = req.params;
+    const { firstname, lastname, anime } = req.body
+
+    const user = users.find((user) =>
+        user.id === id
+    )
+
+    if (firstname) user.firstname === firstname;
+    if (lastname) user.lastname === lastname;
+    if (anime) user.lastname === lastname;
+
+    res.send(`user with id ${id} has been updated`)
 })
 
 
